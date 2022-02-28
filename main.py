54558da3c3a21afb5f67880ff8eeb5a026135f34
Naohiro Tsuji <587586+Naohiro2g@users.noreply.github.com>
2022-02-28 12:34:35 +0900

    col1 and col2, no side-bar

--- main.py
+++ main.py
@@ -12,24 +12,31 @@ BACKGROUND_COLOR = (250, 250, 250, 255)
 st.title('QR Code Generator App')
 st.subheader('You can generate a QR code from a string or URL.')
 st.text('PyQRCodeNG version')
-
 qr_url = st.text_input('Enter a string or URL to generate a QR code:', value='https://code2create.club/')
-qr_version = st.sidebar.slider('Version (1-10)', 1, 10, value=5)
-qr_correction = st.sidebar.select_slider(
-                    "Error Correction Grade: [L, M, Q, H]",
-                    options=['L', 'M', 'Q', 'H'], value='H')
-qr_scale = st.sidebar.slider('Scale (4-8 pixels/cell)', 2, 8, value=4)
+
+col1, col2 = st.columns(2)
+with col1:
+    qr_version = st.slider('Version (1-10)', 1, 10, value=5)
+    qr_correction = st.select_slider(
+                        "Error Correction Grade: [L, M, Q, H]",
+                        options=['L', 'M', 'Q', 'H'], value='H')
+    qr_scale = st.slider('Scale (4-8 pixels/cell)', 2, 8, value=4)
+    st.text(f'Length: {len(qr_url)}')
+    st.text(f'QR Code Version: {qr_version}')
+    st.text(f'Error Correction Grade: {qr_correction}')
+    st.text(f'Mode: Scale: {qr_scale}')
 
 try:
     qr = pyqrcode.create(qr_url, error=qr_correction, version=qr_version, mode='binary')
     qr.png(QR_FILE, scale=qr_scale, module_color=QR_CODE_COLOR, background=BACKGROUND_COLOR, quiet_zone=4)
     img = Image.open(QR_FILE)
-    st.text(f'Length: {len(qr_url)}')
-    st.text(f'[QR Code Version: {qr.version}]   [Error Correction Grade: {qr.error}]   [Mode: {qr.mode}]   [Scale: {qr_scale}]')
-    st.image(img)
+    with col2:
+        st.image(img)
 except pyqrcode.DataOverflowError:
-    st.error(f'Length（文字数）: {len(qr_url)}')
-    st.error('Error: Data Overflow. Please select a smaller version number or a lower Error Correction Grade')
-    st.error('エラー：データあふれ　バージョンやエラー訂正のグレードを下げてください。')
+    with col2:
+        st.error(f'Character length {len(qr_url)} is too long to fit within the QR Code.')
+        st.error('Error: Data Overflow. Please select a smaller version number or a lower Error Correction Grade')
+        st.error('エラー：データあふれ　バージョンやエラー訂正のグレードを下げてください。')
 except Exception as e:
     st.write(e)
+    
